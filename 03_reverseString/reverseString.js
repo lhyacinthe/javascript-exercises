641a1e228a2f6ab970443cc6a62ee44357266ea7
Lindlay <lindlay.hyacinthe@gmail.com>
2022-10-24 19:56:02 +0400

    Start Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    // let letters = [];
+    let newWord = word;
+    let letters = Array.from(newWord);
+    let reversedArray = letters.reverse();
+    return finalWord = reversedArray.toString();
 };
 
 // Do not edit below this line
