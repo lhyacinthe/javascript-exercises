e03d242fc0a02fb34c64c473b6d42cc52ca5d6a9
Lindlay <lindlay.hyacinthe@gmail.com>
2022-10-24 19:04:43 +0400

    Finish Exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let repeatWord = '';
+    if (num<0) {
+        repeatWord = 'ERROR';
+    }
+    else if (string==='') {
+        repeatWord = '';
+    }
+    else {
+        for (let j=0; j<num; j++) {
+            if (j===0) {
+                repeatWord = string;
+            }
+            else {
+                repeatWord += string;
+            }
+        }
+    }
+    return repeatWord;
 };
 
 // Do not edit below this line
