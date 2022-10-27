9e86fb266e315355253b7cd3d6d8db2a653aad1f
Lindlay <lindlay.hyacinthe@gmail.com>
2022-10-27 18:54:46 +0400

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray,...args) {
+    args.forEach((arg) => {
+        const index = originalArray.indexOf(arg);
+        if (index > -1) {
+            originalArray.splice(index,1);
+        }
+    });
+    return originalArray;
 };
 
 // Do not edit below this line
